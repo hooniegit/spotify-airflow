abad67ea07c7cbd965de924941b00869106a560a
hooniegit <neivekim76@gmail.com>
2023-10-17 13:07:07 +0900

    231017 🧑‍🏭 feat: dag 스크립트 오류 수정 및 schedule 지정

---
+++ dags/2_browse_featured_playlists.py
@@ -0,0 +1,80 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.models import Variable
+
+host_fastapi = Variable.get("host_fastapi")
+port_fastapi = Variable.get("port_fastapi")
+
+# execution date 사용
+# 당일 데이터를 호출해야 하므로 + 1일
+# datetime 모듈을 사용하는 것과의 실질적인 차이는 X
+date = "{{ (execution_date + macros.timedelta(days=1)).strftime('%Y-%m-%d') }}"
+
+# default argument 설정
+default_args = {
+    'owner': 'spotify:1.0.0',
+    'depends_on_past': True,
+    'start_date': datetime(2023,10,17)
+}
+
+# dag 설정
+dag = DAG('browse.featured.playlists',
+		  default_args=default_args,
+		  tags=['API', 'MySQL'],
+		  max_active_runs=1,
+		  schedule_interval="5 0 * * *")
+
+# start
+start = EmptyOperator(
+	task_id = 'start',
+	dag=dag
+)
+
+# curl 전송 bash 오퍼레이터
+curl = BashOperator(
+	task_id='curl',
+	bash_command=f"""
+	curl '{host_fastapi}:{port_fastapi}/mysql/featured_playlists?cnt=2'
+	""",
+	dag=dag
+)
+
+# success
+succeed = EmptyOperator(
+	task_id = 'succeed',
+	dag=dag,
+    trigger_rule='all_success'
+)
+
+# failed
+failed = EmptyOperator(
+	task_id = 'failed',
+	dag=dag,
+    trigger_rule='one_failed'
+)
+
+# LINE NOTI 전송 기능
+send_noti = BashOperator(
+    task_id='send.noti',
+    bash_command='''
+    curl -X POST -H 'Authorization: Bearer fxANtArqOzDWxjissz34JryOGhwONGhC1uMN8qc59Z3'
+                 -F '<MESSAGE>' 
+                 https://notify-api.line.me/api/notify
+    ''',
+    dag=dag
+)
+
+# finish
+finish = EmptyOperator(
+	task_id = 'finish',
+	dag = dag,
+    trigger_rule='all_done'
+)
+
+
+# task order
+start >> curl >> [succeed, failed]
+failed >> send_noti
+[succeed, send_noti] >> finish
